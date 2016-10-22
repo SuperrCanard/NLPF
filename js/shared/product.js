919e6007d5a13c2807bc33bea4e69efb0d24ffd5
Steven Ghirighini <ghirig_s@epita.fr>
2016-10-22 19:35:11 +0200

    [ADDED] All javascript classes

---
+++ js/shared/product.js
@@ -0,0 +1,12 @@
+var Product = Class.extend({
+
+    initialize: function (id, name, date, gain, img, description) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.gain = gain;
+        this.img = img;
+        this.description = description;
+    }
+
+});
