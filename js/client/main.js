f403e31df02d3af2b3b0c1b3b9332b0407841a02
Steven Ghirighini <ghirig_s@epita.fr>
2016-10-23 14:31:12 +0200

    [FIXED] Draw order for project

--- js/client/main.js
+++ js/client/main.js
@@ -3,13 +3,18 @@ $(document).ready(function () {
     sendMessage("newProject", { id: 0, name: "test1", gain: 1600, date: '12.10.2016', description: 'Lorem ipsum dolor sit amet', img: './images/windows.jpg' });
     sendMessage("newProject", { id: 1, name: "test2", gain: 1700, date: '12.10.2016', description: 'Lorem ipsum dosdfsdflor sit amet', img: './images/windows.jpg' });
     sendMessage("newProject", { id: 2, name: "test3", gain: 1800, date: '12.10.2016', description: 'Lorem ipsum dodfsdfsdflor sit amet', img: './images/windows.jpg' });
+    sendMessage("newProject", { id: 3, name: "test4", gain: 1900, date: '12.10.2016', description: 'Lorem ipsum dolor sit amet', img: './images/windows.jpg' });
+    sendMessage("newProject", { id: 4, name: "test5", gain: 2000, date: '12.10.2016', description: 'Lorem ipsum dosdfsdflor sit amet', img: './images/windows.jpg' });
+    sendMessage("newProject", { id: 5, name: "test6", gain: 2100, date: '12.10.2016', description: 'Lorem ipsum dodfsdfsdflor sit amet', img: './images/windows.jpg' });
 
     /*** Evenement de réception des projets en temps réel ***/
 
     socket.on('newProject', function (project) {
-        printfObject(project);
+        $('#projectDisplay').prepend($('<span id="project' + project["id"] + '"></span>'));
+
+        //printfObject(project);
         var idButton = "projectButton" + project["id"];
-        $('#projectDisplay').prepend($('<span id="project' + project["id"] + '">'));
+
         displayProject("#project" + project["id"], project, idButton);
 
         var this_project = project;
