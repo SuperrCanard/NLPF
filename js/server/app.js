36489f09ad9b7d97a733da0d2297c8631d4915cc
Steven Ghirighini <ghirig_s@epita.fr>
2016-10-25 00:37:46 +0200

    [ADDED] Hash code for users (security) + some fixes

--- js/server/app.js
+++ js/server/app.js
@@ -1,5 +1,3 @@
-
-
 var app = require('http').createServer(createServer);
 var utils = require('./utils');
 var fs = require('fs'); 
@@ -8,6 +6,7 @@ var io = require('socket.io')(app);
 var projectArray = [];
 var ioArray = [];
 var sessions = [];
+var hashSessions = [];
 
 function createServer(req, res) {
     var path = url.parse(req.url).pathname;
@@ -19,7 +18,6 @@ function createServer(req, res) {
             //console.log(error);
         }
         else {
-
             res.writeHead(200);
             res.write(data);
             res.end();
@@ -57,32 +55,60 @@ function createServer(req, res) {
     }
 }
 
+function makeId(length)
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i = 0; i < length; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 app.listen(8080);
+
+function getSessionByHash(hash) {
+    if (!hash)
+        return -2;
+
+    for (var i = 0; i < hashSessions.length; ++i)
+    {
+        if (hashSessions[i] == hash)
+            return i;
+    }
+
+    return -1;
+}
 
 
 io.on('connection', function (socket) {
     ioArray.push(socket);
     sessions.push({});
+    hashSessions.push(makeId(1024));
 
     var session_id = sessions.length - 1;
     var user_id = ioArray.length - 1;
 
-    console.log("New user: " + user_id);
+    console.log("New user: " + user_id + " (hash = " + hashSessions[session_id] + ")");
 
     /*** Send the session id to the user ***/
 
-    socket.emit("newConnection", session_id);
+    socket.emit("newConnection", hashSessions[session_id]);
 
     /*** Set current user session ***/
 
-    socket.on("newConnection", function (session) {
-        console.log("User '" + user_id + "' has identified as user '" + session + "'");
-        if (session >= sessions.length) {
-            console.log("User '" + session + "' does not exist. Keeping '" + session_id + "'");
-            socket.emit('badConnection', session_id);
+    socket.on("newConnection", function (hashsession) {
+
+        console.log("User sent his hash: " + hashsession);
+        var requested_session = getSessionByHash(hashsession);
+        if (requested_session < 0) {
+            console.log("User with hash '" + hashsession + "' does not exist. Keeping '" + session_id + "'");
+            socket.emit('badConnection', hashSessions[session_id]);
         }
         else {
-            session_id = session;
+            session_id = requested_session;
+            console.log("User '" + user_id + "' has identified as user '" + requested_session + "'");
         }
     });
 
