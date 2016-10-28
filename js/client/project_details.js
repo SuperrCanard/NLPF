91c5b1f267e3378c635d4e7a6d2d159371ed2e18
Steven Ghirighini <ghirig_s@epita.fr>
2016-10-28 15:58:16 +0200

    [ADDED] Project participation unqiue

--- js/client/project_details.js
+++ js/client/project_details.js
@@ -12,12 +12,12 @@ $(document).ready(function () {
         /***[TODO] Modifier les différentes informations de la page en fonction du projet récupéré ***/
         //etc.
 
-        displayProject("#project", this_project);
+        displayProjectDetails("#project", this_project);
 
         sendMessage("getCompensationsByProject", this_project.project_id);
 
         socket.on("getCompensationsByProject", function (result) {
-            printfObject(result);
+
             if (result.success) {
                 for (var i = 0; i < result.results.length; ++i) {
                     var compensation_id = "compensation" + result.results[i].compensation_id;
@@ -26,6 +26,18 @@ $(document).ready(function () {
 
                     var idButton = "CompensationButton" + result.results[i].compensation_id;
                     displayCompensationParticipate("#" + compensation_id, result.results[i], idButton);
+
+                    var this_compensation = result.results[i];
+
+                    $('#' + idButton).click(function () {
+
+                        /***Amener a la page de participation / présentation de projet ***/
+                        session.compensation = this_compensation
+                        session.project = this_project;
+
+                        sendMessage("setSession", session);
+                        window.location = './participate.html';
+                    });
                 }
             }
         });
