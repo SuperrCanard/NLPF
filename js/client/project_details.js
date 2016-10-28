088a2397fbfcf8d1a936e3d2b0779b07ef072fba
Steven Ghirighini <ghirig_s@epita.fr>
2016-10-28 14:56:02 +0200

    [ADDED] Project details display

--- js/client/project_details.js
+++ js/client/project_details.js
@@ -10,10 +10,24 @@ $(document).ready(function () {
         var this_project = session.project;
 
         /***[TODO] Modifier les différentes informations de la page en fonction du projet récupéré ***/
-        $("#projectTitle").text(this_project["name"]);
         //etc.
 
-        displayProjectDetails("#project", this_project);
+        displayProject("#project", this_project);
+
+        sendMessage("getCompensationsByProject", this_project.project_id);
+
+        socket.on("getCompensationsByProject", function (result) {
+            if (result.success) {
+                for (var i = 0; i < result.results.length; ++i) {
+                    var compensation_id = "compensation" + result.results[i].compensation_id;
+
+                    $('#compensations').prepend($('<span id="' + compensation_id + '"></span>'));
+
+                    var idButton = "CompensationButton" + result.results[i].compensation_id;
+                    displayCompensationParticipate("#" + compensation_id, result.results[i], idButton);
+                }
+            }
+        });
 
     });
 
